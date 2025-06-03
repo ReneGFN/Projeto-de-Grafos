e89b32f2ebaa82fc9f88f43ac98e8b02fe83dbe8
Renê <148724999+ReneGFN@users.noreply.github.com>
2025-06-03 16:45:59 -0300

    Adicionar arquivos de projeto.

---
+++ TPGrafos/Program.cs
@@ -0,0 +1,32 @@
+﻿class Aresta
+{
+    public int Inicio;
+    public int Fim;
+    public double Peso;
+    public Aresta(int Inicio, int Fim, double Peso)
+    {
+        this.Inicio = Inicio;
+        this.Fim = Fim;
+        this.Peso = Peso;
+    }
+
+    public override string ToString()
+    {
+        return $"{Inicio} -- {Peso} -> {Fim}";
+    }
+}
+class Grafo
+{
+    int quantVertices;
+    int quantArestas;
+
+    public double[,] matrizAdjacencia;
+    public List<Aresta>;
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
